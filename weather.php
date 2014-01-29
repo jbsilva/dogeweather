b5d9913678f8624262ac186553f61677ffbbbad6
Julio Silva <julio@juliobs.com>
2014-01-29 22:10:45 +0100

    Make weather.php ready to GET a 'city' variable.

--- weather.php
+++ weather.php
@@ -17,22 +17,25 @@ function get_client_ip() {
      return $ipaddress; 
 }
 
-$ip = get_client_ip(); // the IP address to query
+$city = $_GET['city'];
 
-$query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
-if($query && $query['status'] == 'success') {
-  //get Coords
-  $lat = $query['lat'];
-  $lon = $query['lon'];
-
-  $url = "http://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}";
-
-  	$djson = file_get_contents($url);
-	echo $djson;
-
-} else {
-  	$url = "http://api.openweathermap.org/data/2.5/weather?q=Paris";
-  	$djson = file_get_contents($url);
-	echo $djson;
+if ($city) {
+    $url = "http://api.openweathermap.org/data/2.5/weather?q={$city}&cnt=1";
 }
+else {
+    $ip = get_client_ip(); // the IP address to query
+    $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+    if($query && $query['status'] == 'success') {
+      //get Coords
+      $lat = $query['lat'];
+      $lon = $query['lon'];
+      $url = "http://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}";
+    }
+    else {
+        $url = "http://api.openweathermap.org/data/2.5/weather?q=Paris";
+    }
+}
+
+$djson = file_get_contents($url);
+echo $djson;
 ?>
